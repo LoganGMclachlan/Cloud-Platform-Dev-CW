8bc919e710e0ca855ac17c906040bb9208abc643
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-03-25 14:46:36 +0000

    added image analysis functionality

--- analyse_image.py
+++ analyse_image.py
@@ -1,23 +1,32 @@
 import json
+import boto3
+
+client = boto3.client('rekognition')
 
 def lambda_handler(event, context):
-    print("context: " + context)
-    print("event: " + event)
+    result = analyse_image("mybuckets2225362")
+
     return {
         'statusCode': 200,
         'body': json.dumps('Checking image...')
     }
 
-# gets the most recent image uploaded to s3 b ucket, returns image
-def get_image():
-    print("Getting recent image...")
-    return null
-    
 # send image to be analysed, returns results
-def analyse_image(image):
-    print("Analysing image: " + image)
-    return null
-    
+def analyse_image(bucket):
+    images = ["image5","image4","image3","Image2","image1"]
+    # loops through images in reverse order of upload
+    for image in images:
+        # if image referance before it is uploaded, try will catch error
+        try:
+            # tries until most recently uploaded is found
+            response = client.detect_faces(
+                Image={'S3Object': {'Bucket': bucket, 'Name': image}},
+                Attributes=["EMOTIONS","GENDER"])
+            print(response)
+            return response
+        except:
+            pass
+
 # records record of result to dynamoDB
 def record_result(result):
     print("Result recorded.")
