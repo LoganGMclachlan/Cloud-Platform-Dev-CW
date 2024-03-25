4440fe8ce46f65bd2c2e8823d95057ffb6d35601
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-03-25 19:43:37 +0000

    now gets image title from event info

--- analyse_image.py
+++ analyse_image.py
@@ -1,33 +1,39 @@
 import json
 import boto3
 
-client = boto3.client('rekognition')
+rekognition = boto3.client('rekognition')
+s3 = boto3.client("s3")
 
 def lambda_handler(event, context):
-    result = analyse_image("mybuckets2225362")
+    image = get_image(event)
+    result = analyse_image(image,"mybuckets2225362")
 
     return {
         'statusCode': 200,
         'body': json.dumps('Checking image...')
     }
 
-# send image to be analysed, returns results
-def analyse_image(bucket):
-    images = ["image5","image4","image3","Image2","image1"]
-    # loops through images in reverse order of upload
-    for image in images:
-        # if image referance before it is uploaded, try will catch error
-        try:
-            # tries until most recently uploaded is found
-            response = client.detect_faces(
-                Image={'S3Object': {'Bucket': bucket, 'Name': image}},
-                Attributes=["EMOTIONS","GENDER"])
-            print(response)
-            return response
-        except:
-            pass
+# gets image title from sqs event
+def get_image(event):
+    try:
+        # gets most recent image
+        file_obj = json.loads(event["Records"][0]["body"])# converts str body to json onject
+        # gets image title
+        image = file_obj["Records"][0]["s3"]["object"]["key"]
+        return image
+    except:
+        print("Failed to get image.")
+
+# sends image to be analysed, returns results
+def analyse_image(image,bucket):
+    try:
+        # gives recognition service image name and specified attributes to be returned
+        response = rekognition.detect_faces(Image={'S3Object': {'Bucket': bucket, 'Name': image}})
+        print(response)
+        return response
+    except:
+        print("Failed to analyse image.")
 
 # records record of result to dynamoDB
 def record_result(result):
     print("Result recorded.")
-    return null
