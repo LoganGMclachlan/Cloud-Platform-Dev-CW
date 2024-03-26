c46464d12efcea73b20711b9aa64d76a09f081a2
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-03-26 18:41:29 +0000

    faces most confident emotion extracted from result

--- analyse_image.py
+++ analyse_image.py
@@ -3,7 +3,7 @@
 
 # This script will fetch an image from a SQS event,
 # use rekognition to analyse the image, then create
-# a new record of the resutl on a dynamoDB table.
+# a new record of the result on a dynamoDB table.
 
 import json
 import boto3
@@ -13,9 +13,11 @@ rekognition = boto3.client('rekognition')
 s3 = boto3.client("s3")
 db = boto3.client("dynamodb")
 
+# handles the event trigger
 def lambda_handler(event, context):
-    image = get_image(event)
-    result = analyse_image(image,"mybuckets2225362")
+    result = analyse_image(get_image(event))
+    record_result(result)
+    print(result)
 
     return {
         'statusCode': 200,
@@ -26,28 +28,39 @@ def lambda_handler(event, context):
 def get_image(event):
     try:
         # gets most recent image
-        file_obj = json.loads(event["Records"][0]["body"])# converts str body to json object
-        # gets image title
-        image = file_obj["Records"][0]["s3"]["object"]["key"]
-        return image
+        image = json.loads(event["Records"][0]["body"])# converts str body to json object
+        # returns image title/key
+        return image["Records"][0]["s3"]["object"]["key"]
     except:
         print("Failed to get image.")
 
 # sends image to be analysed, returns results
-def analyse_image(image,bucket):
+def analyse_image(image):
     try:
         # gives recognition service image name and specified attributes to be returned
-        response = rekognition.detect_faces(
-            Image={'S3Object': {'Bucket': bucket, 'Name': image}},
+        result = rekognition.detect_faces(
+            Image={'S3Object': {'Bucket': "mybuckets2225362", 'Name': image}},
             Attributes=["EMOTIONS"])
-        return format_result(response)
+        return format_result(result,image)
     except:
         print("Failed to analyse image.")
 
 # extracts important info into readable object
-def format_result(result):
-    formatted = result
-    print(formatted)
+def format_result(result,image):
+    formatted = {"Image":image,"Faces":[]}
+    # loops through every face detected
+    for face in result["FaceDetails"]:
+        # sets most confidant emotion to first option
+        most_confident = face["Emotions"][0]
+        # loops through emotion options
+        for emotion in face["Emotions"]:
+            # checks for most confidant emotion
+            if (emotion["Confidence"] > most_confident["Confidence"]):
+                most_confident = emotion
+        # adds most confidant emotion for a face to formatted list
+        formatted["Faces"].append(most_confident)
+    # final data structure example:
+    # {"Image":"image1","Faces":[{"Type":"HAPPY","Confidance":98.04},{"Type":"SAD","Confidance":94.23}]}
     return formatted
 
 # records record of result to dynamoDB
