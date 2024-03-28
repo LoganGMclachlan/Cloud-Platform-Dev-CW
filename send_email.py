c800285099706eab2433ea0e46d234b9dcd51c23
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-03-28 15:46:28 +0000

    sends email message if angry or frustrated face is found

--- send_email.py
+++ send_email.py
@@ -6,28 +6,45 @@
 # and send email if so.
 
 import json
+import boto3
+
+sns = boto3.client("sns")
 
 # handles the event trigger
 def lambda_handler(event, context):
     for record in event["Records"]:
         # only checks insert type events
         if(record["eventName"] == "INSERT"):
-            # checks for angry face data
-            anger_confidences = record["dynamodb"]["NewImage"]["Angry"]["S"].split(", ")
-            for value in anger_confidences:
-                if(value != "" and float(value) >= 10):
-                    send_email(anger_confidences)
-                    
-            # checks for frustrated face data
-            frustrated_confidences = record["dynamodb"]["NewImage"]["Frustrated"]["S"].split(", ")
-            for value in frustrated_confidences:
-                if(value != "" and float(value) >= 10):
-                    send_email(frustrated_confidences)
+            # gets image data from record
+            image = record["dynamodb"]["NewImage"]
+            key = record["dynamodb"]["Keys"]["PrimaryKey"]["S"]
+            print("Checking " + key + "...")
+            
+            # checks for angry or frustrated emotion data
+            check_emotion("Angry",image,key)
+            check_emotion("Frustrated",image,key)
     
     return {
         'statusCode': 200,
         'body': json.dumps('New item checked.')
     }
 
-def send_email(image):
-    print("Angry face detected in image")
+def check_emotion(emotion,image,key):
+    # checks for face data
+    confidences = image[emotion]["S"].split(", ")
+    for value in confidences:
+        # sends email if confidence reaches 10%
+        if(value != "" and float(value) >= 10):
+            send_email(emotion,value,key)
+
+# sends and email to SNS topic
+def send_email(emotion,value,image):
+    message = "ALERT! " + emotion + " face detected in image " + image
+    print(message)
+    try:
+        sns.publish(TopicArn="arn:aws:sns:us-east-1:891377312372:MyTopics2225362",
+            Message=message,
+            Subject=message)
+        print("Email sent.")
+    except:
+        print("Failed to send email.")
